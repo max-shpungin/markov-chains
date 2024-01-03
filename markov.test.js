222c14151eeca8d1e352c551d62f369d7375374e
Crystal Tran <c.t.tran95@gmail.com>
2024-01-02 21:12:56 -0500

    added TODO's for test file

--- markov.test.js
+++ markov.test.js
@@ -1,7 +1,7 @@
-"use strict";
-
+// TODO: declare test machine so that it's globally available
 const { MarkovMachine } = require('./markov');
 
+// TODO: replace arrow functions with anonymous functions
 describe("get markov chains", ()=>{
 
 
@@ -36,13 +36,16 @@ describe("get markov chains", ()=>{
 
 });
 
-describe('get Markov String', () => {
 
+describe('get Markov String', () => {
+// TODO: indicate the difference between the 2 tests
   test("returns a string", () => {
     testMachine = new MarkovMachine('The cat is in the hat. The cat is the cat. The hat is a cat.');
 
     expect(testMachine.getText()).toEqual(expect.any(String));
   });
+
+  // TODO: check to see if next word is in the array of possibilities
 
   test("returns a markov string", () => {
     testMachine = new MarkovMachine('The cat is in the hat.');
